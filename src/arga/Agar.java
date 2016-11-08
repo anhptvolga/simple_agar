2184ee06f41115e8a112c08988eb05c58f33a370
godric <mr.pt.anh.bsb@gmail.com>
2016-11-08 12:28:25 +0300

    Agar can grow up

--- src/arga/Agar.java
+++ src/arga/Agar.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package arga;
+import com.golden.gamedev.util.ImageUtil;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -17,22 +19,33 @@ import javax.imageio.ImageIO;
  * @author godric
  */
 public class Agar extends com.golden.gamedev.object.Sprite {
-
-    double zoom;
+    private final int GROWTH = 5;
+    private int eatCount;
     
     Agar(BufferedImage image) {
-        super(image);
+        super(ImageUtil.resize(image, 40, 40));
+        
+        eatCount = 0;
     }
 
     Agar(BufferedImage image, double x, double y) {
-        super(image, x-image.getWidth()/2, y-image.getHeight()/2);
+        super(ImageUtil.resize(image, 40, 40),
+                x-image.getWidth()/2, y-image.getHeight()/2);
+        
+        eatCount = 0;
     }
     
-    public void setzoom(double value) {
-        this.zoom = value;
+    public void eat() {
+        this.eatCount ++;
+        this.setImage(ImageUtil.resize(this.getImage(), 
+                this.getWidth() + GROWTH, this.getHeight() + GROWTH));
+    }
+
+    public String toStringEat() {
+        return String.format("Eat %d", this.eatCount);
     }
     
-    public double getzoom() {
-        return this.zoom;
+    public void update(long l) {
+        super.update(l);
     }
 }
